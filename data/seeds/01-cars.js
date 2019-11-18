9b112e5c02ffc4cb65b2caa1704cb6cbd943847a
Toosdai Otte <toosdai52Otte@gmail.com>
2019-11-17 23:31:27 -0600

    seed data added

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,13 +1,12 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('cars').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('cars').insert([
+        { VIN: 'WMWZC5C50DWP31997', make: 'Mini', model: 'Cooper', mileage: 72000, transmission: 'manual', titleStatus: 'clean'},
+        { VIN: '2FDKF37H0PCA09381', make: 'Ford', model: 'F 350', mileage: 138000, transmission: 'automatic', titleStatus: 'salvage'}
       ]);
     });
 };
--- migrations/20191117224709_cars-schema.js
+++ migrations/20191117224709_cars-schema.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cars', table => {
         table.increments('id');
-        table.integer('VIN').unique().notNullable();
+        table.string('VIN', 17).unique().notNullable();
         table.string('make', 100).notNullable();
         table.string('model', 100).notNullable();
         table.integer('mileage').notNullable();
