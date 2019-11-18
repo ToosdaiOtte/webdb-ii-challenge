c44069f8a0bcb4e45eeeeda0a8297eea476b9a86
Toosdai Otte <toosdai52Otte@gmail.com>
2019-11-18 00:31:35 -0600

    MVP done, 1st stretch done

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -17,15 +17,69 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    const {id} = req.params;
 
+    db('car-dealer').where({id}).first()
+    .then(car => {
+        if(car){
+            res.status(200).json(car)
+        } else {
+            res.status(404).json({
+                message: 'Invalid ID'
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Error fetching car from database',
+            err
+        })
+    })
 });
 
 router.post('/', (req, res) => {
+    const carInfo = req.body;
 
+    if(!carInfo.VIN || !carInfo.make || !carInfo.model || !carInfo.mileage){
+        res.status(400).json({
+            message: 'Must include VIN, make, model, and mileage to submit'
+        })
+    } else {
+        db('car-dealer').insert(carInfo)
+        .then(car => {
+            res.status(201).json(car)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error adding car to database',
+                err
+            })
+        })
+    }
 });
 
 router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    const changes = req.body;
 
+    db('car-dealer').where({id}).update(changes)
+    .then(count => {
+        if(count){
+            res.status(200).json({
+                updated: count
+            })
+        } else {
+            res.status(404).json({
+                message: 'Invalid ID'
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Error editing database',
+            err
+        })
+    })
 });
 
 router.delete('/:id', (req, res) => {
